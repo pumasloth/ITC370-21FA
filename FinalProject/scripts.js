2054fe3aa639be5eef3cbf03e4e4d864aec0ed3d
pumasloth <44387089+pumasloth@users.noreply.github.com>
2021-12-01 23:12:44 -0600

    Added documentation and a finish line

--- FinalProject/scripts.js
+++ FinalProject/scripts.js
@@ -5,12 +5,18 @@
     Final Project - The Great Race
 */
 
-// Number of pixels to the finish line.
-const finishLine = 1000;
 const redLight = "images/traffic-light-red.png";
 const greenLight = "images/traffic-light-green.png";
+
+/*
+    Number of pixels to the finish line.
+*/
+var finishLine = (window.innerWidth - (window.innerWidth * .35));
 var winnerDeclared = false;
 
+/*
+    Cycle through a traffic light and either start or reset the race.
+*/
 function ChangeTrafficLightColor() {
     var trafficLight = document.getElementById("trafficLight");
     var trafficLightSrc = trafficLight.getAttribute("src");
@@ -19,10 +25,14 @@ function ChangeTrafficLightColor() {
         StartRace();
     } else if (trafficLightSrc === greenLight) {
         trafficLight.src = redLight;
+        trafficLight.innerHTML = "On your mark..."
         ResetRace();
     }
 }
 
+/*
+    Start the race.
+*/
 function StartRace() {
     winnerDeclared = false;
     MoveParticipant("participant1");
@@ -30,21 +40,32 @@ function StartRace() {
     MoveParticipant("participant3");
 }
 
+/*
+    Reset the race participants back to the start.
+*/
 function ResetRace() {
     ResetParticipant("participant1");
     ResetParticipant("participant2");
     ResetParticipant("participant3");
 }
 
+/*
+    Reset a participant back to the start.
+    @param {string} participantId - The id of the participant.
+*/
 function ResetParticipant(participantId) {
     var participant = document.getElementById(participantId);
     participant.style.left = 0 + "px";
-    participant.onclick = "";
+    participant.removeEventListener("click",  ChangeTrafficLightColor);
 
     var participantText = document.getElementById(participantId + "Text");
     participantText.innerHTML = "";
 }
 
+/*
+    Move a race participant through the race until they cross the finish line.
+    @param {string} participantId - The id of the participant.
+*/
 function MoveParticipant(participantId) {
     var participant = document.getElementById(participantId);
     var position = parseInt(participant.style.left, 10);
@@ -53,16 +74,14 @@ function MoveParticipant(participantId) {
     }
 
     if (position < finishLine) {
-        position += randomNumber(5);
+        position += randomNumber(1, 5);
         participant.style.left = position + "px";
-        setTimeout('MoveParticipant("' + participantId + '")', randomNumber(5));
+        setTimeout('MoveParticipant("' + participantId + '")', randomNumber(1, 5));
     } else {
         if (position >= finishLine && !winnerDeclared) {
             winnerDeclared = true;
             var winnerImg = document.getElementById(participantId);
-            winnerImg.addEventListener("click", function() {
-                ResetRace();
-            });
+            winnerImg.addEventListener("click", ChangeTrafficLightColor);
 
             var winnerText = document.getElementById(participantId + "Text");
             winnerText.innerHTML = "<strong>WINNER!</strong>";
@@ -71,7 +90,11 @@ function MoveParticipant(participantId) {
     }
 }
 
-function randomNumber(max) {
-    var min = 1;
+/*
+    Generate a random number between 1 and max.
+    @param {number} max - The max range number.
+    @returns {number} - The random number generated between 1 (included) and max (excluded).
+*/
+function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
